5a4c0ec5db6f2bf658612d2a670f7dacdd003ca9
Fortunato <fortunato.maycotte@gmail.com>
2017-03-15 01:03:43 -0700

    6

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ Write a function called sum() that takes in two numbers as arguments and then re
 "The sum of 4 and 7 is 11."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
+function su(a,b){
+  return a + b;
+}
 function sum(a,b){
-  var suma = a + b;
+  var suma = su(a,b);
   return [suma,'The sum of ' + a + ' and ' + b + ' is ' + suma + '.'];
 }
 sum(4,7);
@@ -21,15 +23,17 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
+function mult(a,b){
+  return a * b;
+}
 function multiply(a,b){
-  var suma = a * b;
+  var suma = mult(a,b);
   return [suma,'The product of ' + a + ' and ' + b + ' is ' + suma + '.'];
 }
 multiply(5,9);
 testMultiply(5,9);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// Once yu get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
@@ -39,22 +43,15 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// Write your code here
 function sumAndMultiply(a,b,c){
-  var atxt = a.toString();
-  var btxt = b.toString();
-  var ctxt = c.toString();
-  var mult = (a * b) * c;
-  var suma = (a + b) + c;
-  var tot = [mult,suma,atxt + ' * ' + btxt + ' * ' + ctxt,atxt + ' + ' + btxt + ' + ' + ctxt];
-  return tot;
+  var res1 = su(a, b);
+  var res2 = su(res1, c);
+  var res3 = mult(a, b);
+  var res4 = mult(c, res3);
+  return [res2, res4, a + ' and ' + b + ' and ' + c + ' sum to ' + res2 + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + res4 + '.'];
 }
 sumAndMultiply(4,7,5);
-
-//}
-
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
